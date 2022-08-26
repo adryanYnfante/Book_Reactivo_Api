a169ffb54d535192727ebf17f2690848162e4a0e
Adryan Ynfante <109553913+adryanYnfante@users.noreply.github.com>
2022-08-26 08:06:09 -0500

    sin app properties

--- src/main/java/com/sofka/book/controllers/BookController.java
+++ src/main/java/com/sofka/book/controllers/BookController.java
@@ -14,6 +14,44 @@ import java.time.Duration;
 @RestController
 @RequestMapping("/reactive/api/books")
 public class BookController {
+    private final BookService bookService;
 
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Book> getAllBook(){
+        return bookService.getAllBook().delayElements(Duration.ofSeconds(1));
+    }
+
+    @GetMapping(value = "/presure/{nro}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Book> getAllBookBackPresure(@PathVariable int nro){
+        return bookService.getAllBookBackPresure(nro);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Book> findById(@PathVariable String id){
+        return bookService.findById(id);
+    }
+
+    @PostMapping("/")
+    public Mono<Book> postBook(@RequestBody Book book) {
+        return bookService.postBook(book).log();
+    }
+
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<Book>>  updateBook(@PathVariable String id , @RequestBody Book book) {
+        return bookService.updateBook(id,book);
+
+    }
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> deleteBookById(@PathVariable String id){
+        return bookService.deleteUser(id)
+                .map( r -> ResponseEntity.ok().<Void>build())
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
 
 }
